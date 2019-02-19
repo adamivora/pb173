5c5e07130f718d2e4a8af02fa21963940c8826c0
Adam Ivora <xivora@fi.muni.cz>
2019-02-19 21:41:43 +0100

    Test mbedtls

--- src/encrypt.cc
+++ src/encrypt.cc
@@ -1,3 +1,15 @@
+#include <cstdio>
 #include <iostream>
+#include "mbedtls/md.h"
+#include "mbedtls/sha512.h"
 
-int main() { std::cout << "hello, world!\n"; }
+int main() {
+  const unsigned char input[] = "hello world";
+  unsigned char output[64];
+
+  mbedtls_sha512_ret(input, 11, output, 0);
+  for (size_t i = 0; i < sizeof output; i++) {
+    printf("%02x", output[i]);
+  }
+  std::cout << std::endl;
+}
