daffce4b5d0a76c38aa9dc0a7deb739ee63f99ac
Adam Ivora <adam.ivora@gmail.com>
2019-02-19 09:15:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
 #include <stdio.h>
 
-int main(void) { printf("Hello, GitHub!"); }
+int main(void) { 
+  printf("Hello, GitHub!\n");
+  printf("This message was written from the web interface.\n");
+}
